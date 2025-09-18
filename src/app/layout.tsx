0d1b8150df04d6212dff3e0120a765da8d01d651
AIMLD14_Jha Dhruv <Dhruvjha9841@gmail.com>
2025-09-18 15:59:30 +0000

    then make it fast as possible it take too much time in loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,19 @@ import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { AlertProvider } from '@/context/alert-context';
 import { SocialFeedProvider } from '@/context/social-feed-context';
+import { PT_Sans } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: 'BlueVault',
   description: 'A platform for mangrove conservation and carbon credit management.',
 };
+
+const ptSans = PT_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-body',
+});
 
 export default function RootLayout({
   children,
@@ -16,15 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className="font-body antialiased">
+      <body className={cn('font-body antialiased', ptSans.variable)}>
         <AlertProvider>
           <SocialFeedProvider>
             {children}
